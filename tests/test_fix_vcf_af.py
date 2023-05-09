a3364d5cdd37eb5d6d1814cb84567dcbeb4c941c
Niklas Mähler <niklas.mahler@gmail.com>
2023-05-09 08:26:34 +0200

    fix: respect the Number set in the header for AF/AO
    Even though the number for these two should be "A", I have seen cases
    where they are "1", and also cases where one of them is "A" and the
    other is "1". This doesn't really make sense, but the pysam library
    doesn't allow me to modify the header the way I want to (for good
    reason).
    
    Tests were added, and everything looks reasonable.

--- tests/test_fix_vcf_af.py
+++ tests/test_fix_vcf_af.py
@@ -10,30 +10,56 @@ sys.path.insert(0, str(SCRIPT_DIR.resolve()))
 
 from fix_vcf_af import fix_vcf_af, write_variants
 
-@pytest.fixture(scope="session")
-def vcf():
-    return Path("tests/integration/data/HD832_chr7_twist-solid-0.1.5-alpha.vcf")
+
+@pytest.fixture(scope="function")
+def hd832_vcf():
+    vcf_filename = "tests/integration/data/HD832_chr7_twist-solid-0.1.5-alpha.vcf"
+    return pysam.VariantFile(vcf_filename)
 
 
-def test_fix_vcf_af(vcf):
-    variantfile = pysam.VariantFile(vcf)
-    variants = fix_vcf_af(variantfile)
+@pytest.fixture(scope="function")
+def af_ao_vcf():
+    vcf_filename = "tests/integration/data/af_ao.vcf"
+    return pysam.VariantFile(vcf_filename)
 
+
+def test_fix_vcf_af(hd832_vcf, af_ao_vcf):
+    variants = fix_vcf_af(hd832_vcf)
     assert len(variants) == 162
     for v in variants:
         assert "AO" in v.format
         assert "AF" in v.format
         assert "DP" in v.format
 
+    variants = fix_vcf_af(af_ao_vcf)
+    assert len(variants) == 3
+    for v in variants:
+        assert "AO" in v.format
+        assert "AF" in v.format
+        assert "DP" in v.format
 
-def test_write_vcf(vcf, tmpdir):
-    variantfile = pysam.VariantFile(vcf)
-    variants = fix_vcf_af(variantfile)
+    print(variants[0])
+
+    assert variants[0].samples[0]["AO"] == 248
+    assert variants[0].samples[0]["DP"] == 1175
+    assert variants[0].samples[0]["AF"] == pytest.approx(0.2110638298)
+
+    assert variants[1].samples[0]["AO"] is None
+    assert variants[1].samples[0]["DP"] == 1175
+    assert variants[1].samples[0]["AF"] is None
+
+    assert variants[2].samples[0]["AO"] is 248
+    assert variants[2].samples[0]["DP"] is None
+    assert variants[2].samples[0]["AF"] is None
+
+
+def test_write_vcf(hd832_vcf, tmpdir):
+    variants = fix_vcf_af(hd832_vcf)
     out_vcf = tmpdir / "out.vcf"
 
-    write_variants(out_vcf, variants, variantfile.header)
+    write_variants(out_vcf, variants, hd832_vcf.header)
     assert out_vcf.exists
-    
+
     n_variants = 0
     with open(out_vcf) as f:
         for line in f:
--- workflow/scripts/fix_vcf_af.py
+++ workflow/scripts/fix_vcf_af.py
@@ -6,35 +6,55 @@ import sys
 def fix_vcf_af(vcf: VariantFile) -> List[VariantRecord]:
     header = vcf.header
 
-    # Check for AF, DP, and AO in the header
-    if not "AF" in vcf.header.formats:
-        header.formats.add("AF", "A", "Float", "Alternative allele frequency")
+    af_ao_number = "A"
 
-    if not "AO" in vcf.header.formats:
-        header.formats.add("AO", "A", "Integer", "Read depth for each alternative allele")
+    # Set the AF/AO headers if they don't exist, and set the
+    # Number parameter accordintly.
+    if not "AF" in vcf.header.formats and "AO" not in vcf.header.formats:
+        header.formats.add("AF", af_ao_number, "Float", "Allele frequency of each alternative allele")
+        header.formats.add("AO", af_ao_number, "Integer", "Read depth for each alternative allele")
 
+    if "AF" in vcf.header.formats and "AO" in vcf.header.formats:
+        af_ao_number = str(header.formats["AF"].number)
+    elif "AF" in vcf.header.formats:
+        af_ao_number = str(header.formats["AF"].number)
+        header.formats.add("AO", af_ao_number, "Integer", "Alternative allele observations")
+    elif "AO" in vcf.header.formats:
+        af_ao_number = str(header.formats["AO"].number)
+        header.formats.add("AF", af_ao_number, "Float", "Allele frequency of the alternative allele")
+        
     modified_variants = []
 
     for v in vcf:
         if "AO" not in v.format:
             # Cannot tell with the information at hand, set to missing
             v.samples[0]["AO"] = None
+
         if "DP" not in v.format:
             # Cannot tell with the information at hand, set to missing
             v.samples[0]["DP"] = None
-        if "AF" not in v.format:
+
+        if "AF" not in v.format and v.samples[0]["DP"] is not None and v.samples[0]["AO"] is not None:
             # Calculate the alternative allele frequency as the number of
             # reads representing the allele divided by the total read depth
             afs = []
-            if v.samples[0]["AO"][0] is None:
-                afs.append(None)
-            else:
+
+            if af_ao_number == "A":
                 for ao in v.samples[0]["AO"]:
                     afs.append(ao / v.samples[0]["DP"])
-            if len(afs) > 1:
-                v.samples[0]["AF"] = None
+            elif af_ao_number == "1":
+                afs.append(v.samples[0]["AO"] / v.samples[0]["DP"])
             else:
+                raise TypeError(f"invalid Number for AF/AO: {af_ao_number}")
+
+            if len(afs) > 1:
                 v.samples[0]["AF"] = afs
+            elif len(afs) == 1:
+                v.samples[0]["AF"] = afs[0]
+            else:
+                v.samples[0]["AF"] = None
+        else:
+            v.samples[0]["AF"] = None
         
         modified_variants.append(v)
 
