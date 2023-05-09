e3ca851cd3c12270e52e08a0cf30d03605b447ea
Niklas Mähler <niklas.mahler@gmail.com>
2023-05-09 08:30:29 +0200

    style: black formatting
    I should really add precommit to this repo

--- workflow/scripts/fix_vcf_af.py
+++ workflow/scripts/fix_vcf_af.py
@@ -3,6 +3,7 @@ from typing import List, Union
 from pysam import VariantFile, VariantRecord, VariantHeader
 import sys
 
+
 def fix_vcf_af(vcf: VariantFile) -> List[VariantRecord]:
     header = vcf.header
 
@@ -11,18 +12,26 @@ def fix_vcf_af(vcf: VariantFile) -> List[VariantRecord]:
     # Set the AF/AO headers if they don't exist, and set the
     # Number parameter accordintly.
     if not "AF" in vcf.header.formats and "AO" not in vcf.header.formats:
-        header.formats.add("AF", af_ao_number, "Float", "Allele frequency of each alternative allele")
-        header.formats.add("AO", af_ao_number, "Integer", "Read depth for each alternative allele")
+        header.formats.add(
+            "AF", af_ao_number, "Float", "Allele frequency of each alternative allele"
+        )
+        header.formats.add(
+            "AO", af_ao_number, "Integer", "Read depth for each alternative allele"
+        )
 
     if "AF" in vcf.header.formats and "AO" in vcf.header.formats:
         af_ao_number = str(header.formats["AF"].number)
     elif "AF" in vcf.header.formats:
         af_ao_number = str(header.formats["AF"].number)
-        header.formats.add("AO", af_ao_number, "Integer", "Alternative allele observations")
+        header.formats.add(
+            "AO", af_ao_number, "Integer", "Alternative allele observations"
+        )
     elif "AO" in vcf.header.formats:
         af_ao_number = str(header.formats["AO"].number)
-        header.formats.add("AF", af_ao_number, "Float", "Allele frequency of the alternative allele")
-        
+        header.formats.add(
+            "AF", af_ao_number, "Float", "Allele frequency of the alternative allele"
+        )
+
     modified_variants = []
 
     for v in vcf:
@@ -34,7 +43,11 @@ def fix_vcf_af(vcf: VariantFile) -> List[VariantRecord]:
             # Cannot tell with the information at hand, set to missing
             v.samples[0]["DP"] = None
 
-        if "AF" not in v.format and v.samples[0]["DP"] is not None and v.samples[0]["AO"] is not None:
+        if (
+            "AF" not in v.format
+            and v.samples[0]["DP"] is not None
+            and v.samples[0]["AO"] is not None
+        ):
             # Calculate the alternative allele frequency as the number of
             # reads representing the allele divided by the total read depth
             afs = []
@@ -55,13 +68,17 @@ def fix_vcf_af(vcf: VariantFile) -> List[VariantRecord]:
                 v.samples[0]["AF"] = None
         else:
             v.samples[0]["AF"] = None
-        
+
         modified_variants.append(v)
 
     return modified_variants
 
 
-def write_variants(filename: Union[str, pathlib.Path], variants: List[VariantRecord], header: VariantHeader):
+def write_variants(
+    filename: Union[str, pathlib.Path],
+    variants: List[VariantRecord],
+    header: VariantHeader,
+):
     out_vcf = VariantFile(filename, "w", header=header)
     for v in variants:
         out_vcf.write(v)
@@ -77,6 +94,7 @@ def main():
 
     variants = fix_vcf_af(vcf)
     write_variants(output_vcf, variants, vcf.header)
+
 
 if __name__ == "__main__":
     main()
