7ff9b396df3312ff9fb6eabbd5ec2c9bb44ac897
Niklas Mähler <niklas.mahler@gmail.com>
2023-08-29 17:29:13 +0200

    Move panels into package

--- scout_annotation/panels.py
+++ scout_annotation/panels.py
@@ -1,9 +1,13 @@
+import importlib.resources
 import pathlib
 
 
 def get_panels():
-    # TODO: fetch panel directory from config
-    panel_paths = pathlib.Path(__file__).parent.glob("panels/*.tsv")
+    package_dir = importlib.resources.files(__name__.split(".")[0])
+    panel_dir = package_dir.joinpath("data/panels")
+    with importlib.resources.as_file(panel_dir) as pd:
+        panel_paths = pathlib.Path(pd).glob("*.tsv")
+
     panel_dict = {}
     for p in panel_paths:
         with open(p) as f:
