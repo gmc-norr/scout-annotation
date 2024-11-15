7a4c6692af006ec138f32a1c731f286e5d352c86
muabnezor <adam.rosenbaum@regionvasterbotten.se>
2024-11-15 15:03:06 +0100

    Add script for annotation of vcf against civic variants
    - Matches variants based on HGVS.c notation
    - Includes CIVIC_URL in INFO if match is found

--- scout_annotation/workflow/scripts/civic_annotate.py
+++ scout_annotation/workflow/scripts/civic_annotate.py
@@ -3,20 +3,21 @@
 
 from cyvcf2 import VCF, Writer
 import pandas as pd
-from snakemake import snakemake
+import re 
+#from snakemake import snakemake
 
 def annotate_vcf(vcf_path, tsv_path, output_path):
     # Load the VCF file
     vcf = VCF(vcf_path)
 
     # Load Civic TSV file
-    tsv = pd.read_csv(tsv_path, sep='\t')
+    civic_df = pd.read_csv(tsv_path, sep='\t', header=0)
 
     # Add new INFO fields to header
     vcf.add_info_to_header(
         {
-            'ID': 'CIVIC_EVIDENCE',
-            'Description': 'CIVIC evidence annotations',
+            'ID': 'CIVIC_URL',
+            'Description': 'URL for variant in CIVic',
             'Type': 'String',
             'Number': '.'
         }
@@ -25,24 +26,33 @@ def annotate_vcf(vcf_path, tsv_path, output_path):
     writer = Writer(output_path, vcf)
 
     for variant in vcf:
-        rs_id = variant.ID
-        matches = tsv[tsv['rsID'] == rs_id]
-        if not matches.empty:
-            evidence = ','.join(matches['EVIDENCE'].astype(str))
-            variant.INFO['CIVIC_EVIDENCE'] = evidence
+        CSQ_fields = variant.INFO.get("CSQ").split(',')
+        matching_civic = pd.DataFrame(columns=civic_df.columns)
+        for CSQ_field in CSQ_fields:
+            CSQ_dict = {key: value for key,value in zip(format_list, CSQ_field.split('|'))}
+            hgvsc = CSQ_dict.get('HGVSc')
+            matching = civic_df[
+                civic_df['hgvs_descriptions'].notna() &  # Exclude NaN
+                civic_df['hgvs_descriptions'].str.split(',').apply(lambda ids: hgvsc in ids if isinstance(ids, list) else False)
+            ]
+
+            if not matching.empty:
+                matching_civic = pd.concat([matching_civic, matching],axis=0)
+    
+        if not matching_civic.empty:
+            print(matching_civic['variant_civic_url'])
+            variant.INFO['CIVIC_URL'] = matching_civic.iloc[0]['variant_civic_url']
         writer.write_record(variant)
 
     writer.close()
     vcf.close()
 
 def main():
-    vcf = snakemake.input.vcf
-    civic_tsv = snakemake.input.civic_tsv
     annotate_vcf(
         snakemake.input.vcf, 
         snakemake.input.civic_variants_tsv, 
         snakemake.output.vcf
-    )
+    ) 
 
 if __name__ == "__main__":
     main()
